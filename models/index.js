bdd77d477fbe3af6ee9ff3c9f8f52359932cdadd
dibesa <dibesa95@gmail.com>
2016-05-27 18:56:31 +0200

    Tema 27: Añadir imágenes

--- models/index.js
+++ models/index.js
@@ -33,6 +33,9 @@ var Comment = sequelize.import(path.join(__dirname,'comment'));
 // Importar la definicion de la tabla Users de user.js
 var User = sequelize.import(path.join(__dirname,'user'));
 
+// Importar la definicion de la tabla Attachments de attachment.js
+var Attachment = sequelize.import(path.join(__dirname,'attachment'));
+
 // Relaciones entre modelos
 Comment.belongsTo(Quiz);
 Quiz.hasMany(Comment);
@@ -45,6 +48,13 @@ Quiz.belongsTo(User, {as: 'Author', foreignKey: 'AuthorId'});
 User.hasMany(Comment, {foreignKey: 'AuthorId'});
 Comment.belongsTo(User, {as: 'Author', foreignKey: 'AuthorId'});
 
+// Relacion 1-a-1 ente Quiz y Attachment
+Attachment.belongsTo(Quiz);
+Quiz.hasOne(Attachment);
+
+
+
 exports.Quiz = Quiz; // exportar definición de tabla Quiz
 exports.Comment = Comment; // exportar definición de tabla Comments
 exports.User = User;       // exportar definición de tabla Users
+exports.Attachment = Attachment; // exportar definición de tabla Attachments
